b0115adbbaad3ad898125858d50704e7772dedd7
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-01-26 12:57:17 -0300

    Secao 11

---
+++ BMI-11/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI-11
+//
+//  Created by Paola Alcantara Soares on 26/01/22.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
